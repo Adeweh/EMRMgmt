f1f428c4b3f1509f375853805191a831684e47db
Adeweh <dorcasabang@gmail.com>
2022-06-23 18:09:38 +0100

    User Class File

--- src/EMR/User.java
+++ src/EMR/User.java
@@ -1,4 +1,36 @@
 package EMR;
 
+import java.util.ArrayList;
+
 public class User {
+    private int userId;
+    private String name;
+    private int age;
+
+    private static int countUserId = 1;
+
+    private ArrayList<HospitalRecords> hospitalRecords = new ArrayList<>();
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.userId = countUserId;
+        countUserId++;
+
+    }
+
+    public ArrayList<HospitalRecords> getHospitalRecords() {
+        return hospitalRecords;
+    }
+
+
+
+    public void addRecords(String name, int testId, int userId, int doctorsId) {
+        HospitalRecords records = new HospitalRecords(name, testId, userId, doctorsId);
+        hospitalRecords.add(records);
+    }
+
+    public static void resetUserId() {
+        countUserId = 1;
+    }
 }
